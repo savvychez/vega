3c058a1d1ee3646a0aa2a250f99b58e58a70cd4f
savvy chezhian <savvychez@gmail.com>
2024-04-23 00:09:40 +0900

    cuz im a singer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Vega",
+  description: "The best way to find and book appointments with local businesses.",
 };
 
 
